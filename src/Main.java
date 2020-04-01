b731d5c0567aab05b60d83f9bc31052db50da38a
namilan <nichole.milan@gmail.com>
2020-04-01 15:47:23 -0400

    updated

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,14 @@ public class Main {
 		System.out.println("Double minimum value = " +myMinDoubleValue);
 		System.out.println("Double maximum value = " +myMaxDoubleValue);
 		
-		int myIntValue = 5;
-		float myFloatValue = 5.25f;
-		double myDoubleValue = 5.25d;
+		int myIntValue = 5 / 3;
+		float myFloatValue = 5f / 3f;
+		double myDoubleValue = 5.00 / 3;
+		System.out.println("My IntValue = " + myIntValue);
+		System.out.println("MyFloatValue = " + myFloatValue);
+		System.out.println("MyDoubleValue = " + myDoubleValue);
+		
+		
 		
 		
 	}
